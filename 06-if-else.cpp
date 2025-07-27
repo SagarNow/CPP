9f848f304d0cb4c8bf9397809de5c2f814b22822
sagar singh <sagarsingh86199@gmail.com>
2025-07-27 18:35:31 +0530

    if else

---
+++ 06-if-else.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // write a program to take a input of age and print if the person is 18+ or not
+    // >=18  = you are eligible 
+    // <18  = you are not eligible
+    int age ;
+    cin>>age;
+    if (age >=18){
+        cout << "you are eligible";
+    }
+    else {
+        cout << "you are not eligible";
+    }
+
+    return 0;
