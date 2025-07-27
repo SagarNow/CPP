ffa7775f9736e17368fee600e93ab51c149c0a0f
sagar singh <sagarsingh86199@gmail.com>
2025-07-27 17:22:06 +0530

    char

---
+++ 05-char.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //char - single character like a , b , c etc
+    char ch = 'a'; // '' single quote is used for char
+    cout<<ch;
+    return 0;
+}
