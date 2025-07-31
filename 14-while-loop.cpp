8cfc90cec542e65dce25aebb9c02f95491d45e35
sagar singh <sagarsingh86199@gmail.com>
2025-07-31 10:41:27 +0530

    basics of for and while loops

---
+++ 14-while-loop.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // while loop - repeat a block of code like for loop
+   /*  int i  = 1;
+    while (i<=5){
+        cout << "sagar" << " " << i <<'\n';
+        i++;
+    } */
+// -----------------------------------------------------------
+    // do while loop = it will run at least once 
+    int i = 2;
+    do {
+        cout << "sagar" << " " << i <<'\n';
+        i++;
+    } while (i<=1);
+    //1 - i was 2 
+    // then it prints sagar  and i was 2 , then check i<=1 //2 - i was 3 then it stop
+    
+    return 0;
+}
