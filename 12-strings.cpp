8327ab1d0021ef99ce2db75e587f8efe6f16b738
sagar singh <sagarsingh86199@gmail.com>
2025-07-31 10:03:08 +0530

    strings

---
+++ 12-strings.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s = "Sagar";
+    int len = s.length();
+    cout << "length of string is: " << len << "\n";
+    // s[len-1] = 'Z'; // change the last character 
+    cout << s[len-1] << "\n";
+    
+    cout << s[0]; // access the first character
+    cout << s[1]; // access the second character
+    cout << s[2]; // access the third character
+    cout << s[3]; // access the fourth character
+    cout << s[4];  // access the fifth character
+
+    return 0;
+}
