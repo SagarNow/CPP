61b4b1dc37acc39503718da2eae7f96844ceb12b
sagar singh <sagarsingh86199@gmail.com>
2025-07-31 09:54:29 +0530

    2d array

---
+++ 11-2d-array.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    // 2d array = array of arrays like matrix from class 10
+    int arr [3][5];
+    arr [0][0] = 1;
+    arr [0][1] = 2;
+    arr [1][0] = 3;
+    arr [2][0] = 8;
+    
+    cout << arr[0][0];
+    return 0;
+}
